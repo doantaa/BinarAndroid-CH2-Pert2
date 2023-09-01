3adf7cdb6505df827e36ae193912ec9977790d9e
doantaa <doantaaloycius@gmail.com>
2023-09-01 18:06:47 +0700

    Create simple view using view binding

--- app/src/main/java/com/binar/binarch2/MainActivity.kt
+++ app/src/main/java/com/binar/binarch2/MainActivity.kt
@@ -2,10 +2,36 @@ package com.binar.binarch2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+import com.binar.binarch2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var tvHelloWorld: TextView? = null
+
+    // 1. Declare Binding
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // 2. Inisiasi Bindingnya
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // setContentView(R.layout.activity_main)
+        // 3. Ganti nama layout menjadi binding.root
+        setContentView(binding?.root)
+        setTextHello()
+    }
+
+    private fun setTextHello() {
+        // Old Way
+        // tvHelloWorld = findViewById(R.id.tv_hello_world)
+        // tvHelloWorld?.text = "Konnichiwa"
+        // Toast.makeText(this,tvHelloWorld?.text, Toast.LENGTH_SHORT).show()
+
+        // use view binding
+        binding?.tvHelloWorld?.text = "Konnichiwa wawa"
+
     }
 }
